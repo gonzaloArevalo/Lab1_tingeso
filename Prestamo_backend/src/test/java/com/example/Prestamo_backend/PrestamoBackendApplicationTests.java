04b060ff63e7685a56099538b1a74e27a19ad24c
gonzaloArevalo <gonzalo.arevalo@usach.cl>
2024-09-30 11:41:34 -0300

    creacion de carpetas del proyecto

---
+++ Prestamo_backend/src/test/java/com/example/Prestamo_backend/PrestamoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prestamo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

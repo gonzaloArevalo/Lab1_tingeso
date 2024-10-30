c469f51d225606d58cf036c27eb8dde9d4b31969
gonzaloArevalo <gonzalo.arevalo@usach.cl>
2024-10-30 12:59:41 -0300

    inicio d epruebas unitarias

---
+++ Prestamo_backend/src/test/java/com/example/Prestamo_backend/services/UserServiceTest.java
@@ -0,0 +1,8 @@
+package com.example.Prestamo_backend.services;
+
+import com.example.Prestamo_backend.entitites.User;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+public class UserServiceTest {
+    UserService user = new UserService();
+}

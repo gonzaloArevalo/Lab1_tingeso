549845a1d1251f9ef8adbc59d8c073df2bf8436f
gonzaloArevalo <gonzalo.arevalo@usach.cl>
2024-11-03 19:25:32 -0300

    mejora WebConfig de allowedoriginpattern

--- Prestamo_frontend/src/http-common.js
+++ Prestamo_frontend/src/http-common.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const prestamoBackendServer = import.meta.env.VITE_PRESTAMO_BACKEND_SERVER;
-const prestamoBackendPort = import.meta.env.VITE_PRESTAMO_BACKEND_PORT;
+const prestamoBackendPort = 8090;
 
 console.log(prestamoBackendServer)
 console.log(prestamoBackendPort)
